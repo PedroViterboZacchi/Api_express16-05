ee0ec07313f4c7e4aed1b9a6ac489a9e0513e8d8
PedroViterboZacchi <pedro.zacchi@fatec.sp.gov.br>
2025-05-16 19:20:13 +0000

    exemplo express

---
+++ app.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const app = express();
+const port = 3000;
+
+const lista = [
+    { id:1, name: 'john doe'},
+    { id:1, name: 'john doe'}
+];
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+app.get('/', (req, res) => {
+    res.send(lista)
+}
+);
+app.listen(port, () => {
+    console.log('Example app listening on port ${port}')
+}
+);
